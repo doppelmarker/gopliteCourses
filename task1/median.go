98dcbaa75738d7747fc4247c1003e387b00ae0a0
doppelmarker <doppelmarker@gmail.com>
2020-04-13 19:21:50 +0300

    Adding comments to type Square and func End()

--- task1/median.go
+++ task1/median.go
@@ -6,6 +6,7 @@ import (
 )
 
 func median(array []int) float64 {
+	sort.Ints(array)
 	if len(array)%2 != 0 {
 		return float64(array[len(array)/2])
 	}
@@ -13,10 +14,8 @@ func median(array []int) float64 {
 }
 
 func medianTask() {
-	var array1 []int = []int{6, 4, 2, 3, 5, 1} //Even amount of numbers
-	sort.Ints(array1)
+	var array1 []int = []int{6, 4, 2, 3, 5, 1}
 	fmt.Println("Median for even number of elements: ", median(array1))
-	var array2 []int = []int{5, 0, 1, 4, 16} //Odd amount of numbers
-	sort.Ints(array2)
+	var array2 []int = []int{5, 0, 1, 4, 16}
 	fmt.Println("Median for odd number of elements: ", median(array2))
 }
--- task1/square.go
+++ task1/square.go
@@ -6,15 +6,17 @@ type Point struct {
 	x, y int
 }
 
+//Square represents a square whose upper left corner is "start", whereas "a" is its side length
 type Square struct {
 	start Point
 	a     uint
 }
 
+//End is used to find square's bottom right corner
 func (s Square) End() Point {
 	var end Point
-	end.x += int(s.a) + s.start.x
-	end.y -= int(s.a) + s.start.y
+	end.x = int(s.a) + s.start.x
+	end.y = end.y - int(s.a) + s.start.y
 	return end
 }
 
